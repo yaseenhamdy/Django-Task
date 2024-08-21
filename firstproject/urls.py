efb426ea5d446db1299805d32a8e067a2c6c5cc5
yaseenhamdy <yaseenhamdy74@gmail.com>
2024-08-21 18:01:03 +0300

    delete playground file

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/',include('playground.urls')),
     path('api/', include('departments.urls'))  ,
     path('api/', include('employees.urls'))    
      
